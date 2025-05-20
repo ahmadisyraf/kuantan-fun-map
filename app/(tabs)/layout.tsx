bafe611c785b0b0b133f2d132f8ad99729b821ee
ahmadisyraf <isyrafmagic@gmail.com>
2025-05-20 23:59:01 +0800

    update: fix bottom navigation height again

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -7,7 +7,7 @@ export default function TabsLayout({ children }: { children: ReactNode }) {
       <div className="flex-1 overflow-auto h-[calc(100dvh-64px-env(safe-area-inset-bottom))]">
         {children}
       </div>
-      <div className="z-50 w-full h-[calc(64px+env(safe-area-inset-bottom))] pb-[env(safe-area-inset-bottom)] border-t-2 border-black bg-white">
+      <div className="z-50 w-full h-[64px] pb-[env(safe-area-inset-bottom)] border-t-2 border-black bg-white">
         <BottomNavigation />
       </div>
     </div>
