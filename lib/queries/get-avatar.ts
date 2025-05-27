138b0eacf8c0aec6073496e714878e8e0cef3cce
ahmadisyraf <isyrafmagic@gmail.com>
2025-05-27 19:12:40 +0800

    update: fix deploy error

--- lib/queries/get-avatar.ts
+++ lib/queries/get-avatar.ts
@@ -1,7 +1,6 @@
 import { db } from "@/db";
 import { avatar as avatarTable } from "@/db/schema/avatar";
 import { eq } from "drizzle-orm";
-import { unstable_cache } from "next/cache";
 
 async function getAvatar(avatarId: number) {
   const avatar = await db
